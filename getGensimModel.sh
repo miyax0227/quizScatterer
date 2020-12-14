2fb807c2acad6bfa68da0d30c766971e68b65a7f
Ryoh MIYAMOTO <mi_yax@wind.ocn.ne.jp>
2020-12-14 20:19:15 +0900

    gensimモデル取得シェルを追加

---
+++ getGensimModel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd quizScatterer/classes/gensimModel
+if [ $? != 0 ]; then
+  exit 255
+fi
+rm -f *
+wget http://public.shiroyagi.s3.amazonaws.com/latest-ja-word2vec-gensim-model.zip
+unzip latest-ja-word2vec-gensim-model.zip
+rm latest-ja-word2vec-gensim-model.zip
+exit 0
